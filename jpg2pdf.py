12b3ffe5324ab3fd1a07d6a4e95c649101517edc
Aviadg <55297112+Aviadg@users.noreply.github.com>
2019-09-23 13:30:05 -0700

    Update jpg2pdf.py

--- jpg2pdf.py
+++ jpg2pdf.py
@@ -1,26 +1,20 @@
 from fpdf import FPDF
 import os
 import filetype
-from PIL import Image
-
-
-
 
 pdf = FPDF()
 
-# imagelist is the list with all image filenames
-margin = 10
+imageList = []
 
 for image in os.listdir(os.getcwd()):
-	print(type(filetype.guess(image)))
-	if bool(filetype.guess(image)):
-		if filetype.guess(image).extension in ['jpg','png','gif','jpeg']:
-			cover = Image.open(image)
-			width, height = cover.size
-			print(filetype.guess(image).extension)
-			pdf.add_page()
+	if os.path.isfile(image):
+		if bool(filetype.guess(image)):
+			if filetype.guess(image).extension in ['jpg','png','gif','jpeg']:
+				imageList.append(image)
 
-			pdf.image(image, x = 5, y = 5, w = 200, h = 290, type = '', link = '')
+for image in imageList:
+	pdf.add_page()
+	pdf.image(image, x = 5, y = 5, w = 200, h = 290, type = '', link = '')
 
 pdf.output("yourfile.pdf", "F")
 
